da05d8e00c85f48676088d6f92851de1f18438bb
Frank Lehner <frank.lehner@online.de>
2016-12-03 19:15:23 +0100

    bridge.py: Initial commit

---
+++ lib/bridge.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+""" Bridge Pattern """
+
+
+import abc as _abc
+
+
+class Bridge(object):
+    """ Interface """
+    __metaclass__ = _abc.ABCMeta
+
+    @_abc.abstractmethod
+    def operation_imp(self):
+        """ Abstract method operation_imp """
+        return
+
+
+class Abstraction(object):
+    """ Abstraction class """
+    def __init__(self, implementation):
+        """ Constructor """
+        self.bridge = implementation
+
+    def operation(self):
+        """ operation in the abstract way """
+        return "Abstraction <<< BRIDGE >>>>" + self.bridge.operation_imp()
+
+
+class ImplementationA(Bridge):
+    """ child class ImplementationA """
+    def operation_imp(self):
+        """ override """
+        return "ImplementationA"
+
+
+class ImplementationB(Bridge):
+    """ child class ImplementationB """
+    def operation_imp(self):
+        """ override """
+        return "ImplementationB"
